84b47de1341540ce5681ebd1ef482ab86f9bf92b
kristina-0604 <hitman0290@outlook.com>
2021-05-13 22:12:38 +0900

    CMS Dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { CmsController } from './cms/cms.controller';
+import { CmsModule } from './cms/cms.module';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/wash'), CmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
